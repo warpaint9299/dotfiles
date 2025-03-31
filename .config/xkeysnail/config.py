365ce95dffe6b99663295167f84a48421c31e7ee
warpaint <warpaint9299@email.com>
2025-03-31 20:08:52 +0800

    update

--- .config/xkeysnail/config.py
+++ .config/xkeysnail/config.py
@@ -19,20 +19,14 @@ define_keymap(re.compile("firefox-esr|brave-browser|Firefox|Google-chrome"), {
 }, "Browsers")
 
 define_keymap(lambda wm_class: wm_class not in ("kitty", "tmux"), {
-    K("Ctrl-x"): { K("C-x"): K("C-x") },
-    K("Ctrl-a"): { K("C-a"): K("C-a") }
-}, "prefix-binding keys")
-
-define_keymap(lambda wm_class: wm_class not in ("kitty", "tmux"), {
     # Ctrl+d to close tab
     # Ctrl+w/h to delete a word/letter
     K("C-w"): K("C-Backspace"),
     K("C-h"): K("Backspace"),
     # Ctrl+a  moves the cursor to the beginning of a line
     # Ctrl+e  moves the cursor to the end of a line
-    K("C-a"): K("Home"),
+    # K("C-a"): K("Home"),
     K("C-e"): K("End"),
     K("C-t"): K("C-w"),
     K("C-d"): K("C-t"),
-    # Ctrl+Shift+a to select all context
 }, "Terminal-like keys")
